6663e26a608d4852fdea07353b5741c51f93eaeb
Fernando Reyes <fereyji@gmail.com>
2008-02-22 02:16:39 +0000

    Se completan las pruebas unitarias para el modelo Group, se agrega su MVC y sus pruebas funcionales
    git-svn-id: svn+ssh://dev.fisica.unam.mx/svn/inventory@5 5d3da893-9e3c-4fe5-b244-29f3ac4b1555

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -10,21 +10,46 @@ class GroupTest < ActiveSupport::TestCase
     assert_equal 'Las vacas locas', @group.name
   end
 
-  def test_create_bad_name
-    @group = Group.new(:name => 'nil')
-    #assert !@group.valid?
-    #assert !@group.save
-  end
-#    def test_create
-#     @group = Group.new(:name => new_group)
-#    end
-#    def test_read
-#     @group = Group.find(index)
-#    end
-#    def test_update
-#     @group = Group.update(:name = > new_name)
-#    end
-#    def test_delete
-#      @group = Group.destroy(index)
-#    end
+  def test_not_create
+    @group = Group.new(:name => nil)
+    assert !@group.valid?
+    assert !@group.save
+    assert_equal nil, @group.name
+   end
+
+  def test_read
+    @group = Group.find(1)
+    assert @group.valid?
+    assert @group.save
+    assert_equal 1,@group.id
+   end
+
+    def test_update
+      @group = Group.find(2)
+      @group.name = @group.name.reverse
+      assert @group.save
+      assert_equal 'Administrador'.reverse, @group.name
+    end
+
+    def test_not_update_duplicated_name
+      @group = Group.find(2)
+      @group.name = "Default"
+      assert !@group.valid?
+    end
+
+    def test_not_update
+      @group = Group.find(1)
+      @group.name  = nil
+      assert !@group.valid?
+      assert !@group.save
+    end
+
+    def test_delete
+      assert Group.destroy(1)
+      assert_raise (ActiveRecord::RecordNotFound) {  Group.find(1)  }
+    end
+
+    def test_not_delete_nil
+      assert_raise (ActiveRecord::RecordNotFound) {  Group.destroy(nil)  }
+    end
 end
