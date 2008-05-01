e798a939b489fd19cef6e5fae06ead9429fba1a4
Fernando Reyes Jiménez <fereyji@gmail.com>
2008-05-01 13:26:48 -0500

    Se agrega metodo create y se modifica la vista para orden de compra y cambios menores

--- app/controllers/order_requests_controller.rb
+++ app/controllers/order_requests_controller.rb
@@ -11,8 +11,36 @@ class OrderRequestsController < ApplicationController
     1.times{ @order.order_products.build }
   end
 
+<<<<<<< HEAD:app/controllers/order_requests_controller.rb
+  def create
+    @order = Order.new
+    @order.order_status_id = 1
+    @order.user_incharge = User.find(session[:user]).user_incharge.id unless User.find(session[:user]).user_incharge.nil?
+    @order.date = Date.today
+    self.set_user(@order)
+
+    params[:products].each do |p|
+      @product = OrderProduct.new(p)
+      self.set_user(@product)
+      @order.order_products <<  @product
+    end
+
+    respond_to do |format|
+      if @order.save
+        #flash[:notice] = 'Su orden de compra ha sido almacenada'
+        format.html { render :action => "create" }
+        format.xml  { render :xml => @order, :status => :created, :location => @order }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @record.errors, :status => :unprocessable_entity }
+      end
+    end
+   end
+
+=======
   def add_product
     @product =  OrderProduct.new(params[:order_products])
     @order.order_products << @product
   end
+>>>>>>> 086d1987cc6c9bac21df19977a2f4c5ce6117203:app/controllers/order_requests_controller.rb
 end
