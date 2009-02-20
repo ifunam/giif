b5cc0e5dce04e7353107adbc95323724180c5ada
Alejandro Juarez <alexjr85@gmail.com>
2009-02-20 10:21:32 -0600

    Using ActiveResource::Mockhttp instead of flexmocked objects

--- test/unit/authentication_client_test.rb
+++ test/unit/authentication_client_test.rb
@@ -1,9 +1,21 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'flexmock/test_unit'
-class AuthenticationClientTest < ActiveSupport::TestCase
-
-  def test_authenticate
-    flexmock(AuthenticationClient).should_receive(:authenticate?).with('alex','qw12..').and_return(true)
-    assert AuthenticationClient.authenticate?('alex', 'qw12..')
+require 'active_resource'
+require 'active_resource/http_mock'
+class AuthenticationClientTest < Test::Unit::TestCase
+  def setup
+    @valid_auth = { :authentication => true }.to_xml(:root => :user) 
+    @invalid_auth = { :authentication => false }.to_xml(:root => :user) 
+    ActiveResource::HttpMock.respond_to do |mock|
+          mock.get    "/sessions/login.xml?login=juancho&passwd=valid_password", {}, @valid_auth
+          mock.get    "/sessions/login.xml?login=invalid_user&passwd=invalid_password", {}, @invalid_auth
+    end
+  end
+  
+  def test_should_authenticate_valid_users
+    assert AuthenticationClient.authenticate?('juancho', 'valid_password')
+  end
+  
+  def test_should_not_authenticate_invalid_users
+    assert !AuthenticationClient.authenticate?('invalid_user', 'invalid_password')
   end
 end
