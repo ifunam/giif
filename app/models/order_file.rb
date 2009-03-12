359705c59e10c756a3437590f4bd34488796bf55
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-03-12 12:47:41 -0600

    Enhancements on orders/new view

--- app/models/order_file.rb
+++ app/models/order_file.rb
@@ -2,12 +2,4 @@ class OrderFile < ActiveRecord::Base
   validates_presence_of :file_type_id, :file
   belongs_to :order
   belongs_to :file_type
-  
-  after_validation :read_file
-  
-  #def read_file
-  #    content_type = file.content_type
-  #    filename =  file.original_filename
-  #    file = file.class.name
-  #end  
 end
