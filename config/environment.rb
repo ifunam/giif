9c1815da99e2bb74abd3d9a7102bd8e39b5eab24
Alejandro Juarez <alexjr85@gmail.com>
2009-03-18 12:56:23 -0600

    Upgrading gem configuration for each environment

--- config/environment.rb
+++ config/environment.rb
@@ -29,11 +29,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem "sqlite3-ruby", :lib => "sqlite3"
-  #config.gem "rcov"
   config.gem "simple-rss"
-  config.gem "mocha"
-  #config.gem "pdf-writer"
-  
+  config.gem 'pdf-writer', :lib => "pdf/writer"
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
