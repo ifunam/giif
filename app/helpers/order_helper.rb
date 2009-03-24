7f432cfab816e374a746da11e36f5f1279402a71
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-03-23 21:10:04 -0600

    Adding actions partial for views

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -6,4 +6,23 @@ module OrderHelper
   def get_coordinated_for_y(y)
     y == 5 ? "285" : "295"
   end
+
+  def link_to_status(status, title, image_name, options={})
+    case status
+    when false then image_tag(image_name, :title => title)
+    when true  then
+      if title=='Enviar solicitud'
+        link_to_remote(image_tag(image_name, :title => title), options) 
+      else 
+        link_to(image_tag(image_name, :title => title), options) 
+      end
+    end
+  end
+
+  def activate_links(order_status)
+    case order_status
+    when 1 then [true, true, true, true, true]
+    when 3 then [false, false, true, true, false]
+    end
+  end
 end
