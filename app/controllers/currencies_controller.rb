f51d58a8c800581afef28a0b69cb8342323c17b5
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-06-24 12:33:52 -0500

    Currency change feature added

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -18,6 +18,40 @@ class CurrenciesController < ApplicationController
     end
   end
 
+#TODO: Insert this code in a general method
+  def get_currency
+    respond_to do |format|
+      format.js { render :action => 'get_currency.rjs'}
+    end    
+  end
+
+  def submit_currency
+    @record = Currency.find(params[:id])
+
+    if @record.id < 6
+      value = @record.id > 1 ? CurrencyClient.new(@record.url, @record.conversion_title).value : 1
+      session[:currency_order] = CurrencyOrder.new({:value => value, :currency_id => @record.id })
+    end
+
+    respond_to do |format|
+      flash[:notice] = 'La divisa se ha actualizado'
+      format.js { render :action => 'submit_currency.rjs'}
+    end    
+  end
+#TODO
+
+  def update_currency
+    @order_reporter = OrderReporter.find_by_order_id(6) #FIX IT
+    @order = Order.find(6) # FIX IT
+    @order.currency_order.value = session[:currency_order].value
+    @order.currency_order.currency_id = session[:currency_order].currency_id
+
+    respond_to do |format|
+      @order.save
+      format.js { render :action => 'update_currency.rjs'}
+    end    
+  end
+
   def create_currency
     @currency_order = CurrencyOrder.new(:value => params[:currency_order][:value], :currency_id => 6)
     @currency = Currency.new(:name => params[:currency][:name], :url => params[:currency][:url])
