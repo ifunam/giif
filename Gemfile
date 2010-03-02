60002ba30b52c15c170b9a082d5e858982b2a07d
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-03-02 10:19:22 -0600

    Bundler migration, basic gems for giif

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :gemcutter
+
+gem "rails", "2.3.5"
+gem "sqlite3-ruby", :lib => "sqlite3"
+gem "simple-rss"
+gem 'pdf-writer', :lib => "pdf/writer"
+gem "paperclip"
+gem "will_paginate"
+gem "haml"
--- config/environment.rb
+++ config/environment.rb
@@ -1,80 +1,17 @@
-# Be sure to restart your server when you modify this file
-
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
+  config.load_paths += %W( #{RAILS_ROOT}/clients #{RAILS_ROOT}/lib)
+  config.time_zone = 'Mexico City'
+  config.active_record.observers = :order_observer, :order_file_observer if RAILS_ENV != 'test'
 
-  # Skip frameworks you're not going to use. To use Rails without a database
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Specify gems that this application depends on. 
-  # They can then be installed with "rake gems:install" on new installations.
-  # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "simple-rss"
-  config.gem 'pdf-writer', :lib => "pdf/writer"
-
-  # Only load the plugins named here, in the order given. By default, all plugins 
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += %W( #{RAILS_ROOT}/clients)
-  config.load_paths += %W( #{RAILS_ROOT}/lib)
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Make Time.zone default to the specified zone, and make Active Record store time values
-  # in the database in UTC, and return them converted to the specified local zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
-  config.time_zone = 'UTC'
-
-  # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
-  # All files from config/locales/*.rb,yml are added automatically.
-  # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
-
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
+  config.action_mailer.register_template_extension('haml')
+  config.action_mailer.delivery_method = :sendmail
+  sendmail_config = YAML.load_file(RAILS_ROOT + "/config/mail.yml")
+  config.action_mailer.sendmail_settings = sendmail_config['settings']
   config.action_controller.session = {
-    :session_key => '_giif_session',
-    :secret      => 'ee096182965846b40741c6376946a30b05546bf91481c6105ec5cfa7ec760a7fcc86f240effc422b7a16e17d3d0c21c0bf2266f8ca73624ff4ffa8a3390b34bb'
+      :session_key => '_giif_session',
+      :secret      => 'ee096182965846b40741c6376946a30b05546bf91481c6105ec5cfa7ec760a7fcc86f240effc422b7a16e17d3d0c21c0bf2266f8ca73624ff4ffa8a3390b34bb'
   }
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # Please note that observers generated using script/generate observer need to have an _observer suffix
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  config.active_record.observers = :order_observer, :order_file_observer
 end
